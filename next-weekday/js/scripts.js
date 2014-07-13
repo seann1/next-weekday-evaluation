b2cfcb5c0259fce26b6cc1350caafc1c5f254c5a
Sean Niesen <seanniesen@gmail.com>
2014-07-13 12:46:45 -0700

    First working js function
    A function to satisfy the spec which returns the number of days between
    the day input and the current day

---
+++ next-weekday/js/scripts.js
@@ -0,0 +1,32 @@
+
+weekday = function(day) {
+   var dayInput = day.toLowerCase();
+   var today = new Date();
+   var currentDay = today.getDay();
+
+   if (dayInput === "sunday") {
+      var dayNumber = 0
+    } else if (dayInput === "monday") {
+      dayNumber = 1
+    } else if (dayInput === "tuesday") {
+      dayNumber = 2
+    } else if (dayInput === "wednesday") {
+      dayNumber = 3
+    } else if (dayInput === "thursday") {
+      dayNumber = 4
+    } else if (dayInput === "friday") {
+      dayNumber = 5
+    } else if (dayInput === "saturday") {
+      dayNumber = 6
+    }
+
+    var numberOfDays = (dayNumber - currentDay);
+  if (numberOfDays < 0) {
+    numberOfDays = currentDay + dayNumber - 1
+  }
+
+  return numberOfDays
+};
+
+
+
---
+++ next-weekday/specs/specs.js
@@ -0,0 +1,7 @@
+describe("weekday", function() {
+  it ("returns the number of days between the day input and the current day", function () {
+    weekday("Monday").should.equal(1);
+  });
+});
+
+
